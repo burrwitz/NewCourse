fd466ad442aa8d4d8ee294edb7bfb8c658c057d1
George Burr <georgetburr@gmail.com>
2018-11-20 22:17:38 +0000

    Test page

---
+++ js/myJavaScript.js
@@ -0,0 +1,9 @@
+/* if(typeof(Storage)!=="undefined") {
+    // we can accept localStorage
+    alert('localStorage accepted');
+}	else	{
+    // we cannot accept localStorage
+    alert('No localStorage accepted')
+}
+
+alert(localStorage.myName); */
