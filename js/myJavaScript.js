4932a78e784f12d3670d9c800e9e4b319ba29e1f
George Burr <georgetburr@gmail.com>
2018-11-21 21:33:05 +0000

    Updated JavaScript pathing

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,9 +1,7 @@
-/* if(typeof(Storage)!=="undefined") {
+/*if(typeof(Storage)!==undefined) {
     // we can accept localStorage
     alert('localStorage accepted');
 }	else	{
     // we cannot accept localStorage
     alert('No localStorage accepted')
-}
-
-alert(localStorage.myName); */
+};*/
